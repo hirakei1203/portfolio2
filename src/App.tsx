1194747e31149584aef703925f4249c5b1cf48f2
hirakei1203 <hirano.keitaro.64x@gmail.com>
2025-03-28 10:52:45 -0700

    add/thumbnail追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import Earth from "./components/Earth"; 
+import { Helmet } from 'react-helmet';
 
 const App = () => {
   return (
     <div>
+      <Helmet>
+        <meta property="og:title" content="My Portfolio Site" />
+        <meta property="og:description" content="Check out my personal portfolio built with React and Three.js!" />
+        <meta property="og:image" content="https://hirakei1203.github.io/portfolio2/thumbnail.pmg" />
+        <meta property="og:url" content="https://hirakei1203.github.io/portfolio2/" />
+      </Helmet>
       <Earth />
     </div>
   );
