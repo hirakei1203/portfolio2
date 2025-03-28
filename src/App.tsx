2cb7646e587c82f2ee21b214928a97cd3c1378d3
hirakei1203 <hirano.keitaro.64x@gmail.com>
2025-03-28 10:29:16 -0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,8 @@ import { useState } from "react";
 import Earth from "./components/Earth"; 
 
 const App = () => {
-  const [isJapanese, setIsJapanese] = useState(false);
-
   return (
     <div>
-      
       <Earth />
     </div>
   );
