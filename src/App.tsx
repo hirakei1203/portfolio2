b007e86c91faefdb7d8880895b38c71aee3cd5f2
hirakei1203 <hirano.keitaro.64x@gmail.com>
2025-03-28 10:55:11 -0700

    prefix fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
       <Helmet>
         <meta property="og:title" content="My Portfolio Site" />
         <meta property="og:description" content="Check out my personal portfolio built with React and Three.js!" />
-        <meta property="og:image" content="https://hirakei1203.github.io/portfolio2/thumbnail.pmg" />
+        <meta property="og:image" content="https://hirakei1203.github.io/portfolio2/thumbnail.png" />
         <meta property="og:url" content="https://hirakei1203.github.io/portfolio2/" />
       </Helmet>
       <Earth />
