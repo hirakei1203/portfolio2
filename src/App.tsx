4aa13a1b55f2890a131262522e397c546a297883
hirakei1203 <hirano.keitaro.64x@gmail.com>
2025-03-28 10:58:41 -0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ const App = () => {
   return (
     <div>
       <Helmet>
+        <meta property="og:type" content="website" />
         <meta property="og:title" content="My Portfolio Site" />
         <meta property="og:description" content="Check out my personal portfolio built with React and Three.js!" />
         <meta property="og:image" content="https://hirakei1203.github.io/portfolio2/thumbnail.png" />
         <meta property="og:url" content="https://hirakei1203.github.io/portfolio2/" />
+        <meta name="description" content="Check out my personal portfolio built with React and Three.js!" />
       </Helmet>
       <Earth />
     </div>
