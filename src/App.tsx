0eff253f0ef04428c436930063d08b3eb75bcff9
hirakei1203 <hirano.keitaro.64x@gmail.com>
2025-03-29 12:05:27 -0700

    update/thumbnail位置を変更

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,10 @@ import Earth from "./components/Earth";
 import { Helmet } from 'react-helmet';
 
 const App = () => {
-  const description = "Check out my personal portfolio built with React and Three.js!";
-  const imageUrl = "https://hirakei1203.github.io/portfolio2/thumbnail.png";
-  
   return (
     <div>
       <Helmet>
         <title>My Portfolio Site</title>
-        <meta property="og:title" content="My Portfolio Site" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://hirakei1203.github.io/portfolio2/" />
-        <meta property="og:image" content={imageUrl} />
-        <meta property="og:description" content={description} />
-        <meta property="og:site_name" content="My Portfolio Site" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content={imageUrl} />
-        <meta name="twitter:description" content={description} />
-        <meta name="description" content={description} />
       </Helmet>
       <Earth />
     </div>
