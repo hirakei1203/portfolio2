09a3509803e5c1a55de34bd5e77768e9a312ce75
hirakei1203 <hirano.keitaro.64x@gmail.com>
2025-03-29 11:57:04 -0700

    dix

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,23 @@ import Earth from "./components/Earth";
 import { Helmet } from 'react-helmet';
 
 const App = () => {
+  const description = "Check out my personal portfolio built with React and Three.js!";
+  const imageUrl = "https://hirakei1203.github.io/portfolio2/thumbnail.png";
+  
   return (
     <div>
       <Helmet>
-        <meta property="og:type" content="website" />
+        <title>My Portfolio Site</title>
         <meta property="og:title" content="My Portfolio Site" />
-        <meta property="og:description" content="Check out my personal portfolio built with React and Three.js!" />
-        <meta property="og:image" content="https://hirakei1203.github.io/portfolio2/thumbnail.png" />
+        <meta property="og:type" content="website" />
         <meta property="og:url" content="https://hirakei1203.github.io/portfolio2/" />
+        <meta property="og:image" content={imageUrl} />
+        <meta property="og:description" content={description} />
+        <meta property="og:site_name" content="My Portfolio Site" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="description" content="Check out my personal portfolio built with React and Three.js!" />
+        <meta name="twitter:image" content={imageUrl} />
+        <meta name="twitter:description" content={description} />
+        <meta name="description" content={description} />
       </Helmet>
       <Earth />
     </div>
